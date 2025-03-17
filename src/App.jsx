07a1748d56cca8e91f08658decbb6e4ff275f740
KIPYEGON AMOS <kiptoamos@gmail.com>
2025-03-17 15:53:53 +0300

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route,
-}from "react-router-dom";
+} from "react-router-dom";
 import React from "react";
 import Header from "./Header"
 import Footer from "./Footer";
