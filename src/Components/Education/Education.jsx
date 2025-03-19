92006e847ebeb945749ad96916ba5c637776a181
KIPYEGON AMOS <kiptoamos@gmail.com>
2025-03-19 09:30:11 +0300

    portfoli

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -6,18 +6,21 @@ function Education(){
     return(
       <div className='education'>
         <h2>Bachelor of Science in Geospatial Information Science</h2>
+        <h3 className='tit'>Jomo Kenyatta University of Agriculture and Technology</h3>
+
         <div className='edu'>
+
         
             <img src={campo} alt='campo' className='img'></img>
             <div className="caption">
+
             
             <ul>
-                <li className='tit'>Jomo Kenyatta University of Agriculture and Technology</li>
                 <li>September 2017 – December 2021 </li>
                  <li>Final Grade: Second Class Honors - Upper Division </li>
             </ul>
             </div>
-            <div className='caption'>
+            <div className='caption1'>
             <h3 className='tit'>Relevant modules</h3>
             <ul>
                 <li>Geospatial Information Science.</li>
@@ -34,13 +37,14 @@ function Education(){
             </div>
         </div>
         <h2 className='h2w'>Kenya Certificate of Secondary Education </h2>
+        <h3 className='tit'>Lesirwo Secondary School, Kericho </h3>
         <div className='edu'>
 
             <img src={seco} alt='' className='img'></img>
             <div className="caption">
             
             <ul>
-                <li>Lesirwo Secondary School, Kericho </li>
+                
                 
                     <li>February 2013 – November 2016 </li>
                     <li>Mean grad B.</li>
@@ -49,13 +53,14 @@ function Education(){
 
         </div>
         <h2> Kenya Certificate of Primary Education </h2>
+        <h3 className='tit'>Kamachungwa Primary School, Kericho </h3>
         <div className='edu'>
 
             <img src={primo} alt='' className='img'></img>
             <div className="caption">
             
             <ul>
-                <li>Kamachungwa Primary School, Kericho </li>
+                
                 <li>anuary 2004 – November 2012 </li>
                 <li>Final Grade: 257 Marks </li>
             </ul>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from "react";
 import './Navbar.css'
 import { Link } from "react-router-dom";
 import logo from '../../assets/logo.png'
+import { useNavigate } from "react-router-dom";
+
 function Navbar(){
     const [navColor,navBack]=useState(false)
+    const navigate = useNavigate();
     useEffect(()=>{
         window.addEventListener('scroll',()=>{
             window.scrollY>50 ? navBack(true):navBack(false);
@@ -18,7 +21,9 @@ function Navbar(){
         <nav className={`container ${navColor? 'dark-color':''}`}>
             <img className="logo" src={logo} alt="" />
             <h1 className="name">KIPYEGON AMOS</h1>
-            <img onClick={toggleMenu} className="logo1" src={logo} alt=""/>
+            {/* <img onClick={toggleMenu} className="logo1" src={logo} alt=""/> */}
+            
+            <img onClick={() => navigate("/")} className="logo1" src={logo} alt=""/>
            
           
             
