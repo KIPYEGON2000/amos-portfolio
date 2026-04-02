0268b5b315be789f52207fc93cb8f6a9fa51628e
KIPYEGON AMOS <kiptoamos@gmail.com>
2026-04-02 17:51:05 +0300

    home page6

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/amos-portfolio/',  
-  
-});
+  resolve: {
+    alias: {
+      'react-router-dom': path.resolve(__dirname, 'node_modules/react-router-dom')
+    }
+  },
+  server: {
+    fs: {
+      strict: false // This disables the strict file serving restrictions
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom']
+        }
+      }
+    }
+  }
+})
